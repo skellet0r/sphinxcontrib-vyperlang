49760f6402e697387a5237d7036a3b5530c0aae1
Edward Amor <edward.amor3@gmail.com>
2022-10-17 14:55:30 -0400

    fix: add object types to VyperDomain

--- src/sphinxcontrib/vyperlang/domain.py
+++ src/sphinxcontrib/vyperlang/domain.py
@@ -1,4 +1,5 @@
-from sphinx.domains import Domain
+from sphinx.domains import Domain, ObjType
+from sphinx.locale import _
 
 
 class VyperDomain(Domain):
@@ -6,3 +7,15 @@ class VyperDomain(Domain):
 
     name = "vy"
     label = "Vyper"
+    object_types = {
+        "contract": ObjType(_("contract"), "cont", "obj"),
+        "interface": ObjType(_("interface"), "iface", "obj"),
+        "event": ObjType(_("event"), "event", "obj"),
+        "enum": ObjType(_("enum"), "enum", "obj"),
+        "struct": ObjType(_("struct"), "struct", "obj"),
+        "member": ObjType(_("member"), "memb", "obj"),
+        "constant": ObjType(_("constant"), "const", "obj"),
+        "immutable": ObjType(_("immutable"), "immut", "obj"),
+        "statevar": ObjType(_("state variable"), "svar", "obj"),
+        "function": ObjType(_("function"), "func", "obj"),
+    }
