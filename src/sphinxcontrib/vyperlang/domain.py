190d382de6b2cf545914f733deb5d5fcce146b3c
Edward Amor <edward.amor3@gmail.com>
2022-10-19 02:55:45 -0400

    fix: cp _parse_annotation w/ mods

--- src/sphinxcontrib/vyperlang/domain.py
+++ src/sphinxcontrib/vyperlang/domain.py
@@ -1,7 +1,9 @@
+import ast
 import re
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 from docutils import nodes
+from docutils.nodes import Node
 from docutils.parsers.rst import directives
 from sphinx import addnodes
 from sphinx.addnodes import pending_xref
@@ -71,6 +73,118 @@ def type_to_xref(
         refspecific=refspecific,
         **kwargs
     )
+
+
+# copied from sphinx/domains/python.py with minor modifications
+def _parse_annotation(annotation: str, env: BuildEnvironment) -> List[Node]:
+    """Parse type annotation."""
+
+    def unparse(node: ast.AST) -> List[Node]:
+        if isinstance(node, ast.Attribute):
+            return [nodes.Text("%s.%s" % (unparse(node.value)[0], node.attr))]
+        elif isinstance(node, ast.BinOp):
+            result: List[Node] = unparse(node.left)
+            result.extend(unparse(node.op))
+            result.extend(unparse(node.right))
+            return result
+        elif isinstance(node, ast.BitOr):
+            return [
+                addnodes.desc_sig_space(),
+                addnodes.desc_sig_punctuation("", "|"),
+                addnodes.desc_sig_space(),
+            ]
+        elif isinstance(node, ast.Constant):
+            if node.value is Ellipsis:
+                return [addnodes.desc_sig_punctuation("", "...")]
+            elif isinstance(node.value, bool):
+                return [addnodes.desc_sig_keyword("", repr(node.value))]
+            elif isinstance(node.value, int):
+                return [addnodes.desc_sig_literal_number("", repr(node.value))]
+            elif isinstance(node.value, str):
+                return [addnodes.desc_sig_literal_string("", repr(node.value))]
+            else:
+                # handles None, which is further handled by type_to_xref later
+                # and fallback for other types that should be converted
+                return [nodes.Text(repr(node.value))]
+        elif isinstance(node, ast.Expr):
+            return unparse(node.value)
+        elif isinstance(node, ast.Index):
+            return unparse(node.value)
+        elif isinstance(node, ast.Invert):
+            return [addnodes.desc_sig_punctuation("", "~")]
+        elif isinstance(node, ast.List):
+            result = [addnodes.desc_sig_punctuation("", "[")]
+            if node.elts:
+                # check if there are elements in node.elts to only pop the
+                # last element of result if the for-loop was run at least
+                # once
+                for elem in node.elts:
+                    result.extend(unparse(elem))
+                    result.append(addnodes.desc_sig_punctuation("", ","))
+                    result.append(addnodes.desc_sig_space())
+                result.pop()
+                result.pop()
+            result.append(addnodes.desc_sig_punctuation("", "]"))
+            return result
+        elif isinstance(node, ast.Module):
+            return sum((unparse(e) for e in node.body), [])
+        elif isinstance(node, ast.Name):
+            return [nodes.Text(node.id)]
+        elif isinstance(node, ast.Subscript):
+            result = unparse(node.value)
+            result.append(addnodes.desc_sig_punctuation("", "["))
+            result.extend(unparse(node.slice))
+            result.append(addnodes.desc_sig_punctuation("", "]"))
+
+            # Wrap the Text nodes inside brackets by literal node if
+            # the subscript is a Literal
+            if result[0] in ("Literal", "typing.Literal"):
+                for i, subnode in enumerate(result[1:], start=1):
+                    if isinstance(subnode, nodes.Text):
+                        result[i] = nodes.literal("", "", subnode)
+            return result
+        elif isinstance(node, ast.UnaryOp):
+            return unparse(node.op) + unparse(node.operand)
+        elif isinstance(node, ast.Tuple):
+            if node.elts:
+                result = []
+                for elem in node.elts:
+                    result.extend(unparse(elem))
+                    result.append(addnodes.desc_sig_punctuation("", ","))
+                    result.append(addnodes.desc_sig_space())
+                result.pop()
+                result.pop()
+            else:
+                result = [
+                    addnodes.desc_sig_punctuation("", "("),
+                    addnodes.desc_sig_punctuation("", ")"),
+                ]
+
+            return result
+        else:
+            raise SyntaxError  # unsupported syntax
+
+    try:
+        tree = ast.parse(annotation, type_comments=True)
+        result: List[Node] = []
+        for node in unparse(tree):
+            if isinstance(node, nodes.literal):
+                result.append(node[0])
+            elif isinstance(node, nodes.Text) and node.strip():
+                if (
+                    result
+                    and isinstance(result[-1], addnodes.desc_sig_punctuation)
+                    and result[-1].astext() == "~"
+                ):
+                    result.pop()
+                    result.append(type_to_xref(str(node), env, suppress_prefix=True))
+                else:
+                    result.append(type_to_xref(str(node), env))
+            else:
+                result.append(node)
+        return result
+    except SyntaxError:
+        return [type_to_xref(annotation, env)]
 
 
 class VyObject(ObjectDescription):
