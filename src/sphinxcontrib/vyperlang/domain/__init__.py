9b4953b5a22864aa7327d02513fe70624a8c790e
Edward Amor <edward.amor3@gmail.com>
2022-10-20 12:37:42 -0400

    fix: add VyContract directive to VyperDomain

--- src/sphinxcontrib/vyperlang/domain/__init__.py
+++ src/sphinxcontrib/vyperlang/domain/__init__.py
@@ -1,8 +1,11 @@
 from sphinx.domains import Domain
 
+from sphinxcontrib.vyperlang.domain.directives import VyContract
+
 
 class VyperDomain(Domain):
     """Vyper language domain."""
 
     name = "vy"
     label = "Vyper"
+    directives = {"contract": VyContract}
