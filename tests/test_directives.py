f3369f1b98c50c54346bbb64ecd302f3fa0ecfb8
Edward Amor <edward.amor3@gmail.com>
2022-10-20 15:14:39 -0400

    test: VyCurrentContract sets/deletes contract being described

--- tests/test_directives.py
+++ tests/test_directives.py
@@ -4,5 +4,24 @@ from sphinx.testing import restructuredtext
 def test_vy_contract(app):
     text = ".. vy:contract:: sphinx"
     doctree = restructuredtext.parse(app, text)
-    assert doctree.astext() == ""
     assert doctree.settings.env.ref_context.get("vy:contract") == "sphinx"
+
+
+def test_vy_current_contract_updates_contract(app):
+    text = """
+    .. vy:contract:: sphinx
+
+    .. vy:currentcontract:: foo
+    """
+    doctree = restructuredtext.parse(app, text)
+    assert doctree.settings.env.ref_context.get("vy:contract") == "foo"
+
+
+def test_vy_current_contract_clears_contract(app):
+    text = """
+    .. vy:contract:: sphinx
+
+    .. vy:currentcontract:: None
+    """
+    doctree = restructuredtext.parse(app, text)
+    assert doctree.settings.env.ref_context.get("vy:contract") is None
