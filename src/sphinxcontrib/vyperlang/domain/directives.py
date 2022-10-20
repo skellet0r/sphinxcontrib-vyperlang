7217679b6e92291637741cc669ca2e9351a61aba
Edward Amor <edward.amor3@gmail.com>
2022-10-20 12:31:31 -0400

    fix: add VyContract directive class

---
+++ src/sphinxcontrib/vyperlang/domain/directives.py
@@ -0,0 +1,20 @@
+from docutils import nodes
+from sphinx.util.docutils import SphinxDirective, switch_source_input
+from sphinx.util.nodes import nested_parse_with_titles
+
+
+class VyContract(SphinxDirective):
+    """Directive marking the description of a new contract."""
+
+    required_arguments = 1
+    has_content = True
+
+    def run(self):
+        self.env.ref_context["vy:contract"] = self.arguments[0]
+
+        content_node = nodes.section()
+        with switch_source_input(self.state, self.content):
+            content_node.document = self.state.document
+            nested_parse_with_titles(self.state, self.content, content_node)
+
+        return content_node.children
