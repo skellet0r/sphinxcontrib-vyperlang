f7103c6b856ae9bc88f15549ce2115d8dfa7093b
Edward Amor <edward.amor3@gmail.com>
2022-10-21 19:25:10 -0400

    fix: add vy_add_contract_names config entry

--- src/sphinxcontrib/vyperlang/__init__.py
+++ src/sphinxcontrib/vyperlang/__init__.py
@@ -4,4 +4,6 @@ from sphinxcontrib.vyperlang.domain import VyperDomain
 
 
 def setup(app: Sphinx):
+    app.add_config_value("vy_add_contract_names", False, "env")
+
     app.add_domain(VyperDomain)
