f6581fc05c437a37def2ed945b969b3354521247
Edward Amor <edward.amor3@gmail.com>
2022-10-21 01:21:09 -0400

    fix: add generate method to VyperContractIndex

--- src/sphinxcontrib/vyperlang/domain/indices.py
+++ src/sphinxcontrib/vyperlang/domain/indices.py
@@ -1,4 +1,6 @@
-from sphinx.domains import Index
+from typing import Dict, Iterable, List, Tuple
+
+from sphinx.domains import Index, IndexEntry
 from sphinx.locale import _
 
 
@@ -8,3 +10,17 @@ class VyperContractIndex(Index):
     name = "contractindex"
     localname = _("Vyper Contract Index")
     shortname = _("contracts")
+
+    def generate(
+        self, docnames: Iterable[str] = None
+    ) -> Tuple[List[Tuple[str, List[IndexEntry]]], bool]:
+        content: Dict[str, List[IndexEntry]] = {}
+
+        for contract, (docname, node_id) in self.domain.contracts.items():
+            if docnames and docname not in docnames:
+                continue
+
+            entry = IndexEntry(contract, 0, docname, node_id, "", "", "")
+            content.setdefault(contract[0].lower(), []).append(entry)
+
+        return sorted(content.items()), False
