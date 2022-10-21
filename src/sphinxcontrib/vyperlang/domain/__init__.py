d27e23453096d385d54f9e8819d064885ff7f41b
Edward Amor <edward.amor3@gmail.com>
2022-10-21 19:25:06 -0400

    fix: add VyEvent directive + event role to VyperDomain

--- src/sphinxcontrib/vyperlang/domain/__init__.py
+++ src/sphinxcontrib/vyperlang/domain/__init__.py
@@ -10,7 +10,11 @@ from sphinx.roles import XRefRole
 from sphinx.util import logging
 from sphinx.util.nodes import make_refnode
 
-from sphinxcontrib.vyperlang.domain.directives import VyContract, VyCurrentContract
+from sphinxcontrib.vyperlang.domain.directives import (
+    VyContract,
+    VyCurrentContract,
+    VyEvent,
+)
 from sphinxcontrib.vyperlang.domain.indices import VyperContractIndex
 
 logger = logging.getLogger(__name__)
@@ -27,9 +31,16 @@ class VyperDomain(Domain):
 
     name = "vy"
     label = "Vyper"
-    object_types = {"contract": ObjType(_("contract"), "contract")}
-    directives = {"contract": VyContract, "currentcontract": VyCurrentContract}
-    roles = {"contract": XRefRole()}
+    object_types = {
+        "contract": ObjType(_("contract"), "contract"),
+        "event": ObjType(_("event"), "event"),
+    }
+    directives = {
+        "contract": VyContract,
+        "currentcontract": VyCurrentContract,
+        "event": VyEvent,
+    }
+    roles = {"contract": XRefRole(), "event": XRefRole()}
     initial_data: Dict[str, Dict[str, ObjectEntry]] = {"objects": {}}
     indices = [VyperContractIndex]
 
